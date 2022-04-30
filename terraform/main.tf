3dd91246ceb61b6b153915518382f77f3f59e3c6
Wil <molina.wilfredo.j@gmail.com>
2022-04-30 01:39:46 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,9 +4,11 @@ resource "aws_s3_bucket" "apod2" {
 }
 
 resource "aws_s3_object" "apod2" {
+  for_each = fileset("${path.module}/../lambda", "**.py")
+
   bucket = aws_s3_bucket.apod2.id
-  key    = "lambda-test"
-  source = "${path.module}/../lambda/get_image/package.zip"
+  key    = "lambda-${dirname(each.key)}"
+  source = "${path.module}/../lambda/${dirname(each.key)}/package.zip"
 }
 
 # resource "aws_iam_role" "apod2" {
@@ -33,9 +35,9 @@ resource "aws_s3_object" "apod2" {
 
 #   source = "./lambda"
 
-#   function_name = each.value
+#   function_name = each.key
 #   s3_bucket     = aws_s3_bucket.apod2.id
-#   s3_key        = "lambda-${each.value}"
+#   s3_key        = "lambda-${each.key}"
 #   role          = aws_iam_role.apod2.arn
 #   api_id        = aws_apigatewayv2_api.apod2.id
 #   execution_arn = aws_apigatewayv2_api.apod2.execution_arn
