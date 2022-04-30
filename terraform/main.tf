a7e306156d3e1ec7b2d8aa3949a8da720fccada3
Wil <molina.wilfredo.j@gmail.com>
2022-04-30 01:59:45 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,7 @@ resource "aws_s3_bucket" "apod2" {
 }
 
 resource "aws_s3_object" "apod2" {
-  for_each = fileset("${path.module}/../lambda", "**/*.py")
+  for_each = fileset("${path.module}/../lambda", "*/*.py")
 
   bucket = aws_s3_bucket.apod2.id
   key    = "lambda-${dirname(each.value)}"
@@ -31,7 +31,7 @@ resource "aws_apigatewayv2_api" "apod2" {
 }
 
 module "lambda" {
-  for_each = fileset("${path.module}/../lambda", "**/*.py")
+  for_each = fileset("${path.module}/../lambda", "*/*.py")
 
   source = "./lambda"
 
