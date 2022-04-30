bc6a47b2e0dbabc60c1f54c064658a93ba851f19
Wil <molina.wilfredo.j@gmail.com>
2022-04-30 01:12:10 -0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,44 +11,44 @@ resource "aws_s3_object" "apod2" {
   source = "${path.module}/../lambda/${each.value}/package.zip"
 }
 
-resource "aws_iam_role" "apod2" {
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Sid    = ""
-      Principal = {
-        Service = "lambda.amazonaws.com"
-      }
-    }]
-  })
-}
+# resource "aws_iam_role" "apod2" {
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [{
+#       Action = "sts:AssumeRole"
+#       Effect = "Allow"
+#       Sid    = ""
+#       Principal = {
+#         Service = "lambda.amazonaws.com"
+#       }
+#     }]
+#   })
+# }
 
-resource "aws_apigatewayv2_api" "apod2" {
-  name          = "apod2"
-  protocol_type = "HTTP"
-}
+# resource "aws_apigatewayv2_api" "apod2" {
+#   name          = "apod2"
+#   protocol_type = "HTTP"
+# }
 
-module "lambda" {
-  for_each = fileset("${path.module}/../lambda", "*")
+# module "lambda" {
+#   for_each = fileset("${path.module}/../lambda", "*")
 
-  source = "./lambda"
+#   source = "./lambda"
 
-  function_name = each.value
-  s3_bucket     = aws_s3_bucket.apod2.id
-  s3_key        = "lambda-${each.value}"
-  role          = aws_iam_role.apod2.arn
-  api_id        = aws_apigatewayv2_api.apod2.id
-  execution_arn = aws_apigatewayv2_api.apod2.execution_arn
+#   function_name = each.value
+#   s3_bucket     = aws_s3_bucket.apod2.id
+#   s3_key        = "lambda-${each.value}"
+#   role          = aws_iam_role.apod2.arn
+#   api_id        = aws_apigatewayv2_api.apod2.id
+#   execution_arn = aws_apigatewayv2_api.apod2.execution_arn
 
-  depends_on = [
-    aws_s3_object.apod2
-  ]
-}
+#   depends_on = [
+#     aws_s3_object.apod2
+#   ]
+# }
 
-resource "aws_apigatewayv2_stage" "apod2" {
-  api_id      = aws_apigatewayv2_api.apod2.id
-  name        = "apod2"
-  auto_deploy = true
-}
+# resource "aws_apigatewayv2_stage" "apod2" {
+#   api_id      = aws_apigatewayv2_api.apod2.id
+#   name        = "apod2"
+#   auto_deploy = true
+# }
