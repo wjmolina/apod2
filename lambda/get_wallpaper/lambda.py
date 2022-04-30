df5fdd48938a079996eab4d3897b4d36d2f7d235
Wil <molina.wilfredo.j@gmail.com>
2022-04-30 04:24:25 -0500

    tf destroy

--- lambda/get_wallpaper/lambda.py
+++ lambda/get_wallpaper/lambda.py
@@ -5,11 +5,13 @@ import requests
 
 
 def handler(event, context):
-    apod_url = "https://api.nasa.gov/planetary/apod?api_key=w5FXvJgVMYqcUDB4p4ddzcp05j9TGVQ9agL0udxO"
     return {
         "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/html",
-        },
-        "body": boto3.resource("s3").Object("apod2", "index").get()["Body"].read().decode("utf-8").replace("image_url", requests.get(apod_url).json()["hdurl"]),
+        "headers": {"Content-Type": "text/html"},
+        "body": boto3.resource("s3")
+        .Object("apod2", "index")
+        .get()["Body"]
+        .read()
+        .decode("utf-8")
+        .replace("image_url", requests.get("https://api.nasa.gov/planetary/apod?api_key=w5FXvJgVMYqcUDB4p4ddzcp05j9TGVQ9agL0udxO").json()["hdurl"]),
     }
