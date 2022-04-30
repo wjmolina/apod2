c9aaf184720aee9987ce89ac3874ca3554814141
Wil <molina.wilfredo.j@gmail.com>
2022-04-30 04:13:21 -0500

    remove lambda

--- lambda/get_wallpaper/lambda.py
+++ lambda/get_wallpaper/lambda.py
@@ -1,13 +1,15 @@
+import json
+
 import boto3
+import requests
 
 
 def handler(event, context):
-    s3 = boto3.resource("s3")
-    obj = s3.Object("apod2", "index")
+    apod_url = "https://api.nasa.gov/planetary/apod?api_key=w5FXvJgVMYqcUDB4p4ddzcp05j9TGVQ9agL0udxO"
     return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "text/html",
         },
-        "body": obj.get()["Body"].read().decode("utf-8"),
+        "body": boto3.resource("s3").Object("apod2", "index").get()["Body"].read().decode("utf-8").replace("image_url", requests.get(apod_url).json()["hdurl"]),
     }
