4711df747c63c52bac1837b48c2abb8ba2604419
Wil <molina.wilfredo.j@gmail.com>
2022-04-30 02:55:00 -0500

    Update lambda.py

--- lambda/get_wallpaper/lambda.py
+++ lambda/get_wallpaper/lambda.py
@@ -2,12 +2,21 @@ import boto3
 
 
 def handler(event, context):
-    s3 = boto3.resource("s3")
-    obj = s3.Object("apod2", "index")
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "text/html",
-        },
-        "body": obj.get()["Body"].read().decode("utf-8"),
-    }
+    try:
+        s3 = boto3.resource("s3")
+        obj = s3.Object("apod2", "index")
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "text/html",
+            },
+            "body": obj.get()["Body"].read().decode("utf-8"),
+        }
+    except Exception as exception:
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "text/html",
+            },
+            "body": exception,
+        }
